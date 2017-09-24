ac01d48a5dfb0e6b2c5619d81417574f9c744778
Ben Levine <levine@glacier.chemistry.msu.edu>
2017-09-23 20:22:44 -0400

    added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='pyspawn',
+      version='0.0',
+      description='Full Multiple Spawning in Python',
+      author='Benjamin G. Levine',
+      url='https://github.com/blevine37/pySpawn17',
+      packages=['pyspawn', 'pyspawn.classical_integrator', 'pyspawn.potential', 'pyspawn.qm_hamiltonian', 'pyspawn.qm_integrator']
+      )
