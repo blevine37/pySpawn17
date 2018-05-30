20e169f76eff202c6d6fe5e5a7593bd059552141
Dmitry <dfedorov@nevada.unr.edu>
2018-05-30 14:36:53 -0400

    Added propagation of electronic wf. Populations don't seem to change, need to fix it

--- pyspawn/potential/test_cone_td.py
+++ pyspawn/potential/test_cone_td.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
-
+from scipy import linalg as lin
+from Cython.Compiler.PyrexTypes import c_ref_type
 #################################################
 ### electronic structure routines go here #######
 #################################################
@@ -23,50 +24,95 @@ def compute_elec_struct(self,zbackprop):
 
     exec("x = self.get_" + cbackprop + "positions()[0]")
     exec("y = self.get_" + cbackprop + "positions()[1]")
+    
+    n_el_steps = 2000
+    time = self.get_time()
+    el_timestep = self.timestep / n_el_steps
+    a = 6
+    k = 3
+    wf = np.zeros((self.numstates,self.length_wf), dtype = np.complex128)
+
+    # Constructing Hamiltonian
+    H_elec = np.zeros((self.numstates, self.numstates))
+    H_elec[0, 0] = 0.5 * (x + a/2)**2 + 0.5 * (y)**2
+    H_elec[1, 1] = 0.5 * (x - a/2)**2 + 0.5 * (y)**2
+    H_elec[0, 1] = k * y
+    H_elec[1, 0] = k * y
+    print "H_elec = ", H_elec 
+    energies, eigenvectors = lin.eigh(H_elec)
+    print "\neigenvalues = ", energies
+      
     r = math.sqrt( x * x + y * y )
     theta = (math.atan2(y,x)) / 2.0
 
-    e = np.zeros(self.numstates)
-    e[0] = ( r - 1.0 ) * ( r - 1.0 ) - 1.0
-    e[1] = ( r + 1.0 ) * ( r + 1.0 ) - 1.0
-    exec("self.set_" + cbackprop + "energies(e)")
-
+    exec("self.set_" + cbackprop + "energies(energies)")    
+    
+    # Computing forces
     f = np.zeros((self.numstates,self.numdims))
-    ftmp = -2.0 * ( r - 1.0 )
-    f[0,0] = ( x / r ) * ftmp
-    f[0,1] = ( y / r ) * ftmp
-    ftmp = -2.0 * ( r + 1.0 )
-    f[1,0] = ( x / r ) * ftmp
-    f[1,1] = ( y / r ) * ftmp
+    ftmp = - (r - a/2)
+    f[0,0] = (x/r) * ftmp
+    f[0,1] = (y/r) * ftmp
+    ftmp = - (r + a/2)
+    f[1,0] = (x/r) * ftmp
+    f[1,1] = (y/r) * ftmp
     exec("self.set_" + cbackprop + "forces(f)")
+
+    # This part performs the propagation of the electronic wave function for ehrenfest dynamics
+    if time < 1e-8: # first time step electronic wave function propagated only by dt/2:
+        print "\nPropagating first iteration:"
+        # Constructing electronic wave function for the first timestep
+        prev_wf = np.zeros((self.numstates, self.length_wf), dtype=np.complex128)
+        prev_wf[0,0] = math.sin(theta) / np.sqrt(2)
+        prev_wf[0,1] = math.cos(theta) / np.sqrt(2)
+        prev_wf[1,0] = math.cos(theta) / np.sqrt(2)
+        prev_wf[1,1] = -math.sin(theta) / np.sqrt(2)
+#         print "\nprev_wf =", prev_wf
+#         print "\nprev_norm = ", np.dot(np.transpose(np.conjugate(prev_wf[:, 0])), prev_wf[:, 0]) + np.dot(np.transpose(np.conjugate(prev_wf[:, 1])), prev_wf[:, 1])
         
-    wf = np.zeros((self.numstates,self.length_wf))
-    wf[0,0] = math.sin(theta)
-    wf[0,1] = math.cos(theta)
-    wf[1,0] = math.cos(theta)
-    wf[1,1] = -math.sin(theta)
-    exec("prev_wf = self.get_" + cbackprop + "prev_wf()")
-    # phasing wave function to match previous time step
+        wf = propagate_symplectic(self, H_elec, prev_wf, el_timestep/2, n_el_steps/2) 
+           
+    else: # propagation for not the first step
+        
+        exec("prev_wf = self.get_" + cbackprop + "prev_wf()")
+        wf = propagate_symplectic(self, H_elec, prev_wf, el_timestep, n_el_steps)
+    
+    eigenvectors_t = np.transpose(np.conjugate(eigenvectors))    
+    amp_0 = np.dot(eigenvectors_t[0, :], wf)
+    pop_0 = np.dot(np.transpose(np.conjugate(amp_0)), amp_0)
+    print "\nwf =", wf
+    print "\n0th state population:", pop_0
+    print "\nnorm = ", np.dot(np.transpose(np.conjugate(wf[:, 0])), wf[:, 0]) + np.dot(np.transpose(np.conjugate(wf[:, 1])), wf[:, 1])
+
+#     phasing wave function to match previous time step
     W = np.matmul(prev_wf,wf.T)
+     
     if W[0,0] < 0.0:
         wf[0,:] = -1.0 * wf[0,:]
         W[:,0] = -1.0 * W[:,0]
+     
     if W[1,1] < 0.0:
         wf[1,:] = -1.0 * wf[1,:]
         W[:,1] = -1.0 * W[:,1]
-    
-    # computing NPI derivative coupling
-    tmp = self.compute_tdc(W)
-    tdc = np.zeros(self.numstates)
-    if self.istate == 1:
-        jstate = 0
-    else:
-        jstate = 1
-    tdc[jstate] = tmp
-    exec("self.set_" + cbackprop + "timederivcoups(tdc)")
-     
+        
     exec("self.set_" + cbackprop + "wf(wf)")
 
+def propagate_symplectic(self, H, wf, timestep, nsteps):
+
+    c_r = np.real(wf)
+    c_i = np.imag(wf)
+            
+    for i in range(nsteps):
+        
+        c_r_dot = np.dot(H, c_i)
+        c_r = c_r + 0.5 * timestep * c_r_dot
+        c_i_dot = -1.0 * np.dot(H, c_r)
+        c_i = c_i + timestep * c_i_dot  
+        c_r_dot = np.dot(H, c_i)
+        c_r = c_r + 0.5 * timestep * c_r_dot  
+    
+    wf = c_r + 1j * c_i
+    return wf
+    
 def init_h5_datasets(self):
     self.h5_datasets["time"] = 1
     self.h5_datasets["energies"] = self.numstates
