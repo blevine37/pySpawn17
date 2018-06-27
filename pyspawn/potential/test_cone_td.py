ca322726681601923453eb51fb3b4c46ce918931
Dmitry <dfedorov@nevada.unr.edu>
2018-06-27 14:48:39 -0400

    Modeling ehrenfest dynamics on 2 state model

--- pyspawn/potential/test_cone_td.py
+++ pyspawn/potential/test_cone_td.py
@@ -20,8 +20,8 @@ def compute_elec_struct(self,zbackprop):
     else:
         cbackprop = "backprop_"
 
-    exec("self.set_" + cbackprop + "prev_wf(self.get_" + cbackprop + "wf())")
-
+#     exec("self.set_" + cbackprop + "prev_wf(self.get_" + cbackprop + "wf())")
+    prev_wf = self.td_wf_real + 1j*self.td_wf_imag
     exec("x = self.get_" + cbackprop + "positions()[0]")
     exec("y = self.get_" + cbackprop + "positions()[1]")
     
@@ -30,7 +30,6 @@ def compute_elec_struct(self,zbackprop):
     el_timestep = self.timestep / n_el_steps
     a = 6
     k = 3
-    wf = np.zeros((self.numstates,self.length_wf), dtype = np.complex128)
 
     # Constructing Hamiltonian
     H_elec = np.zeros((self.numstates, self.numstates))
@@ -38,68 +37,79 @@ def compute_elec_struct(self,zbackprop):
     H_elec[1, 1] = 0.5 * (x - a/2)**2 + 0.5 * (y)**2
     H_elec[0, 1] = k * y
     H_elec[1, 0] = k * y
-    print "\nH_elec = ", H_elec 
+#     print "\nH_elec = ", H_elec 
     energies, eigenvectors = lin.eigh(H_elec)
-    print "\neigenvalues = ", energies
+#     print "\neigenvalues = ", energies
       
     r = math.sqrt( x * x + y * y )
     theta = (math.atan2(y,x)) / 2.0
 
-    exec("self.set_" + cbackprop + "energies(energies)")    
-    
-    # Computing forces
-    f = np.zeros((self.numstates, self.numdims))
-    ftmp = - (r + a/2)
-    f[0,0] = (x/r) * ftmp
-    f[0,1] = (y/r) * ftmp
-    ftmp = - (r - a/2)
-    f[1,0] = (x/r) * ftmp
-    f[1,1] = (y/r) * ftmp
-    exec("self.set_" + cbackprop + "forces(f)")
+    exec("self.set_" + cbackprop + "energies(energies)")        
 
     # This part performs the propagation of the electronic wave function for ehrenfest dynamics
     if time < 1e-8: # first time step electronic wave function propagated only by dt/2:
         print "\nPropagating first iteration:"
 
         # Constructing electronic wave function for the first timestep
-        prev_wf = np.zeros((self.numstates, self.length_wf), dtype=np.complex128)
-        prev_wf[0,0] = math.sin(theta) / np.sqrt(2)
-        prev_wf[0,1] = math.cos(theta) / np.sqrt(2)
-        prev_wf[1,0] = math.cos(theta) / np.sqrt(2)
-        prev_wf[1,1] = -math.sin(theta) / np.sqrt(2)
-       
+        prev_wf[0] = math.sin(theta) 
+        prev_wf[1] = math.cos(theta) 
+
         wf = propagate_symplectic(self, H_elec, prev_wf, el_timestep/2, n_el_steps/2) 
            
     else: # propagation for not the first step
-        
-        exec("prev_wf = self.get_" + cbackprop + "prev_wf()")
+
         wf = propagate_symplectic(self, H_elec, prev_wf, el_timestep, n_el_steps)
     
+    # Computing forces
+    Hx = np.zeros((self.numstates, self.numstates))
+    Hx[0, 0] = x + a/2
+    Hx[1, 1] = x - a/2
+    
+    Hy = np.zeros((self.numstates, self.numstates))
+    Hy[0, 0] = y
+    Hy[1, 1] = y
+    Hy[0, 1] = k
+    Hy[1, 0] = k
+    
+    f = np.zeros((self.numstates, self.numdims))    
+    prev_wf_T = np.transpose(np.conjugate(prev_wf))
+#     print "F=", np.dot(np.dot(prev_wf_T, Hx), prev_wf), np.dot(np.dot(prev_wf_T, Hy), prev_wf)
+    f[0, 0] = np.real(np.dot(np.dot(prev_wf_T, Hx), prev_wf))
+    f[0, 1] = np.real(np.dot(np.dot(prev_wf_T, Hy), prev_wf))
+    
+    exec("self.set_" + cbackprop + "forces(f)")
+    
+    pop = np.zeros(self.numstates) 
     eigenvectors_t = np.transpose(np.conjugate(eigenvectors))    
     amp_0 = np.dot(eigenvectors_t[0, :], wf)
-    pop_0 = np.dot(np.transpose(np.conjugate(amp_0)), amp_0)
+    pop[0] = np.real(np.dot(np.transpose(np.conjugate(amp_0)), amp_0))
     amp_1 = np.dot(eigenvectors_t[1, :], wf)
-    pop_1 = np.dot(np.transpose(np.conjugate(amp_1)), amp_1)
+    pop[1] = np.real(np.dot(np.transpose(np.conjugate(amp_1)), amp_1))
     
-    print "\nwf =", wf
-    print "\namp_0 = ", amp_0
-    print "1st state population:", pop_0
-    print "2nd state population:", pop_1
-    print "norm = ", pop_0 + pop_1
+#     print "prev_wf =", prev_wf
+#     print "\nwf =", wf
+#     print "\namp_0 = ", amp_0
+    print "\n1st state population:", pop[0]
+    print "2nd state population:", pop[1]
+    print "norm = ", pop[0] + pop[1]
     print ""
-#     phasing wave function to match previous time step
-    W = np.matmul(prev_wf,wf.T)
-     
-    if W[0,0] < 0.0:
-        wf[0,:] = -1.0 * wf[0,:]
-        W[:,0] = -1.0 * W[:,0]
-     
-    if W[1,1] < 0.0:
-        wf[1,:] = -1.0 * wf[1,:]
-        W[:,1] = -1.0 * W[:,1]
-        
-    exec("self.set_" + cbackprop + "wf(wf)")
 
+#     phasing wave function to match previous time step
+#     W = np.matmul(prev_wf,wf.T)
+#      
+#     if W[0,0] < 0.0:
+#         wf[0,:] = -1.0 * wf[0,:]
+#         W[:,0] = -1.0 * W[:,0]
+#      
+#     if W[1,1] < 0.0:
+#         wf[1,:] = -1.0 * wf[1,:]
+#         W[:,1] = -1.0 * W[:,1]
+
+    self.td_wf_real = np.real(wf)
+    self.td_wf_imag = np.imag(wf)
+    self.populations = pop
+
+    
 def propagate_symplectic(self, H, wf, timestep, nsteps):
 
     c_r = np.real(wf)
@@ -118,24 +128,27 @@ def propagate_symplectic(self, H, wf, timestep, nsteps):
     return wf
     
 def init_h5_datasets(self):
+    self.h5_datasets["td_wf_real"] = self.numstates
+    self.h5_datasets["td_wf_imag"] = self.numstates
     self.h5_datasets["time"] = 1
     self.h5_datasets["energies"] = self.numstates
     self.h5_datasets["positions"] = self.numdims
     self.h5_datasets["momenta"] = self.numdims
     self.h5_datasets["forces_i"] = self.numdims
-    self.h5_datasets["wf0"] = self.numstates
-    self.h5_datasets["wf1"] = self.numstates
+    self.h5_datasets["populations"] = self.numstates
+#     self.h5_datasets["wf0"] = self.numstates
+#     self.h5_datasets["wf1"] = self.numstates
     self.h5_datasets_half_step["time_half_step"] = 1
     self.h5_datasets_half_step["timederivcoups"] = self.numstates
 
 def potential_specific_traj_copy(self,from_traj):
     return
 
-def get_wf0(self):
-    return self.wf[0,:].copy()
-
-def get_wf1(self):
-    return self.wf[1,:].copy()
+# def get_wf0(self):
+#     return self.wf[0,:].copy()
+# 
+# def get_wf1(self):
+#     return self.wf[1,:].copy()
 
 def get_backprop_wf0(self):
     return self.backprop_wf[0,:].copy()
