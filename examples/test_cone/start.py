Fixed the electronic population formula, also tweaked some parameters in the linear slope model

examples/test_cone/start.py
@@ -19,10 +19,10 @@ potential = "linear_slope"
 t0 = 0.0
 
 # time step
-ts = 0.05
+ts = 0.1
 
 # final simulation time
-tfinal = 100.0
+tfinal = 80.0
 
 # number of dimensions                                                                                           
 numdims = 1
@@ -45,12 +45,15 @@ traj_params = {
     # nuclear masses (in a.u)    
     "masses": np.asarray([1822.0]),
     # initial positions
-    "positions": np.asarray([-0.5]),
+    "positions": np.asarray([-0.2]),
     # inition momenta
-    "momenta": np.asarray([30.0]),
+    "momenta": np.asarray([10.0]),
 #     "momenta": np.asarray([0.3]),
 #     "numdims": numdims,
-    #
+    # Use approximate eigenstates or full Hamiltonian diagonalization
+    "full_H": True,
+    # Size of Krylov subspace for full_H = False 
+#     "krylov_sub_n": 4,
     "numstates": numstates,
     # How many electronic timesteps in one nuclear (default = 1000)
     "n_el_steps": 1000,    
@@ -68,14 +71,13 @@ sim_params = {
     # energy shift used in quantum propagation
     "qm_energy_shift": 0.0,
     # cloning probability threshold
-    "p_threshold": 0.17,
+    "p_threshold": 0.014,
     # cloning minimum population parameter
-    "pop_threshold": 0.1,
+    "pop_threshold": 0.05,
     # type of cloning procedure:
     # "toastate" : cloning on to a state energy of which is different from average
     # "pairwise" : considering each pair, transferring population between them
     "cloning_type": "toastate",
-
 }
 
 # import routines needed for propagation

pyspawn/fafile.py
@@ -200,6 +200,9 @@ class fafile(object):
         
         ntraj = self.get_num_traj()
         times = self.datasets["quantum_times"][:, 0]
+        el_pop = self.h5file["sim/el_pop"][:]
+        self.datasets["el_pop"] = el_pop
+
         ntimes = len(times)
         Nstate = np.zeros((ntimes, ntraj+1))
         norm = np.zeros((ntimes))
@@ -227,7 +230,6 @@ class fafile(object):
 #             print "error =", norm[i] - sum_pop
 #         print "S =", S_t
         self.datasets["nuclear_bf_populations"] = Nstate
-    
         if column_filename != None:
             self.write_columnar_data_file("quantum_times", ["nuclear_bf_populations"], column_filename)
 
@@ -256,6 +258,29 @@ class fafile(object):
                 self.write_columnar_data_file(key + "_time",\
                                               [dset_pop],\
                                               column_filename)        
+ 
+    def fill_approx_el_populations(self, column_filename=None):
+        """Calculating electronic state populations assuming no overlap between trajectories
+        NOT FINISHED!!!"""
+        
+        ntraj = self.get_num_traj()
+        times = self.datasets["quantum_times"][:, 0]
+        ntimes = len(times)
+        last_time = times[ntimes - 1]
+        Nstate = np.zeros((ntimes, ntraj+1))
+        n_approx_states = self.get_traj_dataset("00", krylov_sub_n)
+        norm = np.zeros((n_approx_states))
+        
+        for i in range(n_approx_states):
+            nt = self.ntraj[last_time]
+            c_t = self.get_amplitude_vector(last_time)
+            for n in range(nt):
+                norm[i] += np.real(np.dot(np.transpose(np.conjugate(c_t[n])), c_t[n]))\
+                * self.traj_map[n].approx_pop[i]
+
+        self.datasets["approx_el_populations"] = norm
+
+        return 
     
     def write_xyzs(self):
         """Prints out geometries into .xyz file"""

pyspawn/fmsobj.py
@@ -46,6 +46,7 @@ class fmsobj(object):
 
     def from_dict(self, **tempdict):
         """Convert dict structure to fmsobj structure"""
+        
         for key in tempdict:
             if isinstance(tempdict[key], types.UnicodeType) :
                 tempdict[key] = str(tempdict[key])
@@ -56,7 +57,8 @@ class fmsobj(object):
                 if isinstance((tempdict[key])[0], types.StringTypes) :
                     if (tempdict[key])[0][0] == "^":
                         for i in range(len(tempdict[key])):
-                            tempdict[key][i] = eval(tempdict[key][i])
+                            print "key =", key
+                            tempdict[key][i] = eval(tempdict[key][i][1:])
                         tempdict[key] = np.asarray(tempdict[key], dtype=np.complex128)
 #new
                     if isinstance((tempdict[key])[0], types.UnicodeType) :
@@ -99,12 +101,12 @@ class fmsobj(object):
         self.from_dict(**tempdict)
 
     def set_parameters(self, params):
-        print "### Setting " + self.__class__.__name__ + " parameters"
+        print "\nSetting " + self.__class__.__name__ + " parameters:"
         for key in params:
             print key + " = " + str(params[key]) 
             if hasattr(self, key):
                 exec("self." + key + "= params[key]")
             else:
-                print "### Parameter " + key + " not found in " + self.__class__.__name__ + ", exiting"
+                print "Parameter " + key + " not found in " + self.__class__.__name__ + ", exiting"
                 quit()
                 

pyspawn/potential/linear_slope.py
@@ -22,6 +22,8 @@ def compute_elec_struct(self):
     ehrenfest wave function one nuclear time step that's split into 
     smaller electronic timesteps"""
     
+    n_krylov = self.krylov_sub_n
+    
     wf = self.td_wf
     print "Performing electronic structure calculations:"
     x = self.positions[0]
@@ -30,7 +32,8 @@ def compute_elec_struct(self):
     time = self.time
     el_timestep = self.timestep / n_el_steps
 
-    # Constructing Hamiltonian, computing derivatives, for now solving the eigenvalue problem to get adiabatic states
+    # Constructing Hamiltonian, computing derivatives, for now solving the eigenvalue problem
+    # to get adiabatic states
     # for real systems it will be replaced by approximate eigenstates
     H_elec, Force = self.construct_el_H(x) 
     ss_energies, eigenvectors = lin.eigh(H_elec)
@@ -38,20 +41,34 @@ def compute_elec_struct(self):
     
     pop = np.zeros(self.numstates)
     amp = np.zeros((self.numstates), dtype=np.complex128) 
+    approx_pop = np.zeros(self.krylov_sub_n)
+    approx_amp = np.zeros((self.krylov_sub_n), dtype=np.complex128)
         
     if np.dot(np.transpose(np.conjugate(wf)), wf)  < 1e-8:
         print "WF = 0, constructing electronic wf for the first timestep", wf
-        wf = eigenvectors[:, 2]
+        wf = eigenvectors[:, 4]
 #         wf = 1 /np.sqrt(self.numstates) * sum(eigenvectors[:, n] for n in range(self.numstates)) 
     else:
         if not self.first_step:
             print "\nPropagating electronic wave function not first step"
-            wf = propagate_symplectic(self, (H_elec), wf, self.timestep/2, n_el_steps/2)
+            wf = propagate_symplectic(self, (H_elec), wf, self.timestep/2, n_el_steps/2,\
+                                      n_krylov)
+            self.wf_store_full_ts = self.wf_store.copy()
+            
         if self.first_step:
             print "\nFirst step, skipping electronic wave function propagation"
-    
+            symplectic_backprop(self, H_elec, wf, el_timestep, n_krylov, n_krylov)
+
     wf_T = np.transpose(np.conjugate(wf))
     av_energy = np.real(np.dot(np.dot(wf_T, H_elec), wf))    
+
+    q, r = np.linalg.qr(self.wf_store_full_ts)
+    Hk = np.dot(np.transpose(np.conjugate(q)), np.dot(H_elec, q))
+    Fk = np.dot(np.transpose(np.conjugate(q)), np.dot(Force, q))
+    
+    approx_e, approx_eigenvecs = np.linalg.eigh(Hk)
+    self.approx_energies = approx_e
+    self.approx_eigenvecs = approx_eigenvecs
     
     av_force = np.zeros((self.numdims))    
     for n in range(self.numdims):
@@ -62,15 +79,27 @@ def compute_elec_struct(self):
         pop[j] = np.real(np.dot(np.transpose(np.conjugate(amp[j])), amp[j]))
     norm = np.dot(wf_T, wf)
     
+    approx_wf = np.dot(np.conjugate(np.transpose(q)), wf)
+    
+    for j in range(self.krylov_sub_n):
+        approx_amp[j] = np.dot(approx_eigenvecs[:, j], approx_wf)
+        approx_pop[j] = np.real(np.dot(np.transpose(np.conjugate(approx_amp[j])), approx_amp[j]))
+    
+    self.approx_amp = approx_amp
+    self.approx_pop = approx_pop    
+    self.approx_wf_full_ts = np.complex128(approx_wf)
     self.av_energy = float(av_energy)
     self.energies = np.real(ss_energies)
     self.av_force = av_force
-    self.approx_eigenvecs = eigenvectors
+    self.eigenvecs = eigenvectors
     self.mce_amps_prev = self.mce_amps
     self.mce_amps = amp
     self.td_wf_full_ts = np.complex128(wf)
     self.populations = pop
-
+    if self.momenta[0] < 0:
+        print "The trajectory reached the inflection point: exiting"
+        sys.exit()
+    
     if abs(norm - 1.0) > 1e-6:
         print "WARNING: Norm is not conserved!!! N =", norm  
     
@@ -84,11 +113,16 @@ def compute_elec_struct(self):
 #         print "momentum =", self.momenta
 #         print "H_elec =\n", H_elec
         print "Average energy =", self.av_energy
+#         print "wf_store =", self.wf_store_full_ts
         print "Energies =", ss_energies
 #         print "Force =", av_force
-        print "Wave function =\n", wf
-        print "Eigenvectors =\n", eigenvectors
-        print "Population = ", pop 
+#         print "Wave function =\n", wf
+        print "approx wf =\n", self.approx_wf_full_ts
+        print "approx amp", self.approx_amp
+        print "approx_e =", approx_e
+#         print "approx Eigenvectors =\n", self.approx_eigenvecs
+        print "approx Population = ", approx_pop
+        print "population =", self.populations 
         print "norm =", sum(pop)
 #         print "amps =", amp
     print_stuff()
@@ -96,7 +130,8 @@ def compute_elec_struct(self):
     
     # This part performs the propagation of the electronic wave function 
     # for ehrenfest dynamics at a half step and save it
-    wf = propagate_symplectic(self, H_elec, wf, self.timestep/2, n_el_steps/2)
+    wf = propagate_symplectic(self, H_elec, wf, self.timestep/2, n_el_steps/2, n_krylov)
+
     self.td_wf = wf
     self.H_elec = H_elec
     
@@ -115,32 +150,76 @@ def construct_el_H(self, x):
     """Constructing the 2D potential (Jahn-Teller model) and computing d/dx, d/dy for
     force computation. Later will be replaced with the electronic structure program call"""
     
-    # 3 state 1D system ##############################
+    # 2 state 1D system ##############################
+#     a = 6
+#     k = 0.005
+#     w = 0.25
+#     delta = 0.01
+# #       
+#     H_elec = np.zeros((self.numstates, self.numstates))
+#     H_elec[0, 0] = w * (-x) 
+#     H_elec[1, 1] = w * x 
+#     H_elec[1, 0] = k
+#     H_elec[0, 1] = k
+#     H_elec[2, 2] = w * x - delta
+#       
+#     H_elec[2, 1] = k * x
+#     H_elec[2, 0] = k * x
+#          
+#     H_elec[1, 2] = k * x
+#     H_elec[0, 2] = k * x
+#       
+#     Hx = np.zeros((self.numstates, self.numstates))
+#     Hx[0, 0] = -w
+#     Hx[1, 1] = w
+#     Hx[2, 2] = w
+#       
+#     Hx[2, 1] = k
+#     Hx[2, 0] = k
+#     Hx[1, 2] = k
+#     Hx[0, 2] = k
+
+    # 5 state 1D system ##############################
     a = 6
-    k = 0.5
-    w = 0.5
-    delta = 0.1
-      
+    k = 0.005
+    w1 = 0.25
+    w2 = 0.025
+    delta = 0.025
+       
     H_elec = np.zeros((self.numstates, self.numstates))
-    H_elec[0, 0] = w * (-x) 
-    H_elec[1, 1] = w * x 
-    H_elec[2, 2] = w * x - delta
-      
-    H_elec[2, 1] = k * x
-    H_elec[2, 0] = k * x
-         
-    H_elec[1, 2] = k * x
-    H_elec[0, 2] = k * x
-      
+    H_elec[0, 0] = w1 * (-x) 
+    H_elec[1, 1] = w2 * x 
+    H_elec[2, 2] = w2 * x - delta
+    H_elec[3, 3] = w2 * x - 2 * delta
+    H_elec[4, 4] = w2 * x - 3 * delta
+#     
+#       
+    H_elec[0, 1] = k #* x
+    H_elec[0, 2] = k #* x
+    H_elec[0, 3] = k #* x     
+    H_elec[0, 4] = k #* x
+     
+    H_elec[1, 0] = k #* x
+    H_elec[2, 0] = k #* x
+    H_elec[3, 0] = k #* x     
+    H_elec[4, 0] = k # * x
+#       
     Hx = np.zeros((self.numstates, self.numstates))
-    Hx[0, 0] = -w
-    Hx[1, 1] = w
-    Hx[2, 2] = w
-      
-    Hx[2, 1] = k
-    Hx[2, 0] = k
-    Hx[1, 2] = k
-    Hx[0, 2] = k
+    Hx[0, 0] = -w1
+    Hx[1, 1] = w2
+    Hx[2, 2] = w2
+    Hx[3, 3] = w2 
+    Hx[4, 4] = w2 
+     
+#     Hx[0, 1] = k
+#     Hx[0, 2] = k
+#     Hx[0, 3] = k
+#     Hx[0, 4] = k
+#      
+#     Hx[1, 0] = k
+#     Hx[2, 0] = k
+#     Hx[3, 0] = k
+#     Hx[4, 0] = k
 
     ####################################################
     
@@ -161,13 +240,14 @@ def construct_el_H(self, x):
     
     return H_elec, Force
 
-def propagate_symplectic(self, H, wf, timestep, nsteps):
+def propagate_symplectic(self, H, wf, timestep, nsteps, n_krylov):
     """Symplectic split propagator, similar to classical Velocity-Verlet"""
     
+#     approx_eig = np.zeros((self.numstates, n_krylov), dtype = np.complex128)
     el_timestep = timestep / nsteps
     c_r = np.real(wf)
     c_i = np.imag(wf)
-            
+    n = 0        
     for i in range(nsteps):
         
         c_r_dot = np.dot(H, c_i)
@@ -176,10 +256,40 @@ def propagate_symplectic(self, H, wf, timestep, nsteps):
         c_i = c_i + el_timestep * c_i_dot  
         c_r_dot = np.dot(H, c_i)
         c_r = c_r + 0.5 * el_timestep * c_r_dot  
-    
+        
+        if nsteps - i <= n_krylov:
+            self.wf_store[:, n] = c_r + 1j * c_i 
+            n += 1
+#     print "wf_store =\n", self.wf_store   
+#     print "wf_store_full_ts =", self.wf_store_full_ts
     wf = c_r + 1j * c_i
-    
+#     print "wf =", wf
     return wf
+ 
+def symplectic_backprop(self, H, wf, el_timestep, nsteps, n_krylov):
+    """Symplectic split propagator, similar to classical Velocity-Verlet"""
+    
+#     approx_eig = np.zeros((self.numstates, n_krylov), dtype = np.complex128)
+    c_r = np.real(wf)
+    c_i = np.imag(wf)
+    n = 0        
+    self.wf_store_full_ts = np.zeros((self.numstates, self.krylov_sub_n), dtype = np.complex128)
+    for n in range(nsteps):
+        
+        c_r_dot = np.dot(H, c_i)
+        c_r = c_r - 0.5 * el_timestep * c_r_dot
+        c_i_dot = -1.0 * np.dot(H, c_r)
+        c_i = c_i - el_timestep * c_i_dot  
+        c_r_dot = np.dot(H, c_i)
+        c_r = c_r - 0.5 * el_timestep * c_r_dot  
+    
+        self.wf_store_full_ts[:, n_krylov-n-1] = c_r + 1j * c_i 
+    
+#     print "wf_store =\n", self.wf_store   
+#     print "wf_store_full_ts =\n", self.wf_store_full_ts
+    self.wf_store = self.wf_store_full_ts.copy()
+#     print "wf =", wf
+    return 
     
 def init_h5_datasets(self):
     

pyspawn/potential/test_cone_td.py
@@ -20,6 +20,8 @@ def compute_elec_struct(self):
     This subroutine solves the electronic SE and propagates electronic 
     ehrenfest wave function one nuclear time step that's split into 
     smaller electronic timesteps"""
+
+    n_krylov = self.krylov_sub_n
     
     wf = self.td_wf
     print "Performing electronic structure calculations:"
@@ -42,16 +44,44 @@ def compute_elec_struct(self):
         
     if np.dot(np.transpose(np.conjugate(wf)), wf)  < 1e-8:
         print "WF = 0, constructing electronic wf for the first timestep", wf
-        wf = eigenvectors[:, 1]
+        wf = eigenvectors[:, 4]
+#         wf = 1 /np.sqrt(self.numstates) * sum(eigenvectors[:, n] for n in range(self.numstates)) 
     else:
         if not self.first_step:
             print "\nPropagating electronic wave function not first step"
-            wf = propagate_symplectic(self, (H_elec), wf, self.timestep/2, n_el_steps/2)
+            wf = propagate_symplectic(self, (H_elec), wf, self.timestep/2, n_el_steps/2,\
+                                      n_krylov)
+            self.wf_store_full_ts = self.wf_store.copy()
+            
         if self.first_step:
             print "\nFirst step, skipping electronic wave function propagation"
+            symplectic_backprop(self, H_elec, wf, el_timestep, n_krylov, n_krylov)
     
     wf_T = np.transpose(np.conjugate(wf))
     av_energy = np.real(np.dot(np.dot(wf_T, H_elec), wf))    
+
+    ########### Approximate eigenstates ########################
+    q, r = np.linalg.qr(self.wf_store_full_ts)
+    Hk = np.dot(np.transpose(np.conjugate(q)), np.dot(H_elec, q))
+    Fk = np.dot(np.transpose(np.conjugate(q)), np.dot(Force, q))
+    
+    approx_e, approx_eigenvecs = np.linalg.eigh(Hk)
+    self.approx_energies = approx_e
+    self.approx_eigenvecs = approx_eigenvecs
+    
+    approx_wf = np.dot(np.conjugate(np.transpose(q)), wf)
+    approx_pop = np.zeros(self.krylov_sub_n)
+    approx_amp = np.zeros((self.krylov_sub_n), dtype=np.complex128)
+        
+    for j in range(self.krylov_sub_n):
+        approx_amp[j] = np.dot(approx_eigenvecs[:, j], approx_wf)
+        approx_pop[j] = np.real(np.dot(np.transpose(np.conjugate(approx_amp[j])), approx_amp[j]))
+
+    self.approx_amp = approx_amp
+    self.approx_pop = approx_pop    
+    self.approx_wf_full_ts = np.complex128(approx_wf)
+
+    ############################################################
     
     av_force = np.zeros((self.numdims))    
     for n in range(self.numdims):
@@ -96,7 +126,7 @@ def compute_elec_struct(self):
     
     # This part performs the propagation of the electronic wave function 
     # for ehrenfest dynamics at a half step and save it
-    wf = propagate_symplectic(self, H_elec, wf, self.timestep/2, n_el_steps/2)
+    wf = propagate_symplectic(self, H_elec, wf, self.timestep/2, n_el_steps/2, n_krylov)
     self.td_wf = wf
     self.H_elec = H_elec
         
@@ -117,36 +147,61 @@ def construct_el_H(self, pos):
     
     x = pos[0]
     y = pos[1]
-    a = 6
+    a = 1
     k = 1
-    w = 0.5
+    w = 0.4
+    delta = 0.3
     
     H_elec = np.zeros((self.numstates, self.numstates))
-    H_elec[0, 0] = w * (x + a/2)**2 + w * (y)**2
-    H_elec[1, 1] = w * (x - a/2)**2 + w * (y)**2
+    H_elec[0, 0] = w * (x)**2 + w * (y)**2
+    H_elec[1, 1] = w * (x - a/2)**2 + w * (y)**2 - delta
+    H_elec[2, 2] = w * (x - 2*a/2)**2 + w * (y)**2 - 2*delta
+    H_elec[3, 3] = w * (x - 3*a/2)**2 + w * (y)**2 - 3*delta
+    H_elec[4, 4] = w * (x - 4*a/2)**2 + w * (y)**2 - 4*delta
     H_elec[0, 1] = k * y
     H_elec[1, 0] = k * y
+    H_elec[0, 2] = k * y
+    H_elec[2, 0] = k * y
+    H_elec[0, 3] = k * y
+    H_elec[3, 0] = k * y
+    H_elec[0, 4] = k * y
+    H_elec[4, 0] = k * y
 
     Hx = np.zeros((self.numstates, self.numstates))
     Hy = np.zeros((self.numstates, self.numstates))
 #     Hz = np.zeros((self.numstates, self.numstates))
-    Hx[0, 0] = 2 * w * (x + a/2)
+    Hx[0, 0] = 2 * w * (x)
     Hx[1, 1] = 2 * w * (x - a/2)
+    Hx[2, 2] = 2 * w * (x - 2*a/2)
+    Hx[3, 3] = 2 * w * (x - 3*a/2)
+    Hx[4, 4] = 2 * w * (x - 4*a/2)
+    
     Hy[0, 0] = 2 * w * y
     Hy[1, 1] = 2 * w * y
+    Hy[2, 2] = 2 * w * y
+    Hy[3, 3] = 2 * w * y
+    Hy[4, 4] = 2 * w * y
+    
     Hy[0, 1] = k
     Hy[1, 0] = k
+    Hy[0, 2] = k
+    Hy[2, 0] = k
+    Hy[0, 3] = k
+    Hy[3, 0] = k
+    Hy[0, 4] = k
+    Hy[4, 0] = k
     
     Force = [Hx, Hy]    
     return H_elec, Force
 
-def propagate_symplectic(self, H, wf, timestep, nsteps):
+def propagate_symplectic(self, H, wf, timestep, nsteps, n_krylov):
     """Symplectic split propagator, similar to classical Velocity-Verlet"""
     
+#     approx_eig = np.zeros((self.numstates, n_krylov), dtype = np.complex128)
     el_timestep = timestep / nsteps
     c_r = np.real(wf)
     c_i = np.imag(wf)
-            
+    n = 0        
     for i in range(nsteps):
         
         c_r_dot = np.dot(H, c_i)
@@ -155,11 +210,41 @@ def propagate_symplectic(self, H, wf, timestep, nsteps):
         c_i = c_i + el_timestep * c_i_dot  
         c_r_dot = np.dot(H, c_i)
         c_r = c_r + 0.5 * el_timestep * c_r_dot  
-    
+        
+        if nsteps - i <= n_krylov:
+            self.wf_store[:, n] = c_r + 1j * c_i 
+            n += 1
+#     print "wf_store =\n", self.wf_store   
+#     print "wf_store_full_ts =", self.wf_store_full_ts
     wf = c_r + 1j * c_i
-    
+#     print "wf =", wf
     return wf
+
+def symplectic_backprop(self, H, wf, el_timestep, nsteps, n_krylov):
+    """Symplectic split propagator, similar to classical Velocity-Verlet"""
     
+#     approx_eig = np.zeros((self.numstates, n_krylov), dtype = np.complex128)
+    c_r = np.real(wf)
+    c_i = np.imag(wf)
+    n = 0        
+    self.wf_store_full_ts = np.zeros((self.numstates, self.krylov_sub_n), dtype = np.complex128)
+    for n in range(nsteps):
+        
+        c_r_dot = np.dot(H, c_i)
+        c_r = c_r - 0.5 * el_timestep * c_r_dot
+        c_i_dot = -1.0 * np.dot(H, c_r)
+        c_i = c_i - el_timestep * c_i_dot  
+        c_r_dot = np.dot(H, c_i)
+        c_r = c_r - 0.5 * el_timestep * c_r_dot  
+    
+        self.wf_store_full_ts[:, n_krylov-n-1] = c_r + 1j * c_i 
+    
+#     print "wf_store =\n", self.wf_store   
+#     print "wf_store_full_ts =\n", self.wf_store_full_ts
+    self.wf_store = self.wf_store_full_ts.copy()
+#     print "wf =", wf
+    return
+   
 def init_h5_datasets(self):
     
     self.h5_datasets["av_energy"] = 1

pyspawn/simulation.py
@@ -15,6 +15,7 @@ import datetime
 import time
 import cmath 
 import copy
+from astropy.coordinates.builtin_frames.utils import norm
 
 class simulation(fmsobj):
     
@@ -29,9 +30,10 @@ class simulation(fmsobj):
 
         # olapmax is the maximum overlap allowed for a spawn.  Above this,
         # the spawn is cancelled
-        self.olapmax = 0.6
+        self.olapmax = 0.1
         
         # if pij > p_threshold  and pop > pop_threshold cloning is initiated 
+        self.num_el_states = 5
         self.pop_threshold = 0.1
         self.p_threshold = 0.03
         self.cloning_type = "toastate"
@@ -51,7 +53,7 @@ class simulation(fmsobj):
 
         # quantum amplitudes
         self.qm_amplitudes = np.zeros(0,dtype=np.complex128)
-
+        self.el_pop = np.zeros(self.num_el_states)
         # energy shift for quantum propagation
         self.qm_energy_shift = 0.0
 
@@ -170,6 +172,7 @@ class simulation(fmsobj):
             
             # propagate quantum variables if possible
             print "\nPropagating quantum amplitudes if we have enough information to do so"
+            
             self.propagate_quantum_as_necessary()
 
             # moved cloning routine into the propagation to update h5 file on time
@@ -179,8 +182,11 @@ class simulation(fmsobj):
             # print restart output - this must be the last line in this loop!
             print "Updating restart output"
             self.restart_output()
-            print "Elapsed wall time: %6.1f" % (time.clock() - t0)    
-        
+            print "Elapsed wall time: %6.1f" % (time.clock() - t0)
+                
+#             if np.shape(self.S)[0] > 1:
+#                 print "S12 =", np.abs(self.S[0,1])
+#             
     def propagate_quantum_as_necessary(self):
         """here we will propagate the quantum amplitudes if we have
         the necessary information to do so.
@@ -211,12 +217,14 @@ class simulation(fmsobj):
 
             print "\nOutputing quantum information to hdf5"
             self.h5_output()
-            
+            self.calc_approx_el_populations()
             print "\nNow we will clone new trajectories if necessary:"
             
             if self.cloning_type == "toastate":
                 self.clone_to_a_state()
                 if self.clone_again:
+                    for key in self.traj:
+                        self.traj[key].compute_cloning_E_diff()
                     self.clone_to_a_state()
             
             if self.cloning_type == "pairwise":
@@ -623,7 +631,6 @@ class simulation(fmsobj):
                             # Number overlap elements that are larger than threshold
                             s = (S > self.olapmax).sum()
                             print "SUM =", s
-                            
                             if s > new_dim:
                                 
                                 print "Aborting cloning due to large overlap with\
@@ -668,13 +675,22 @@ class simulation(fmsobj):
                 """Do we clone to istate?
                 If the following conditions are satisfied then we clone to that state
                 and we're done. If done we go to another state with lower probability"""
-                
+                if self.traj[key].full_H:
+                    pop_to_check = self.traj[key].populations
+                else:
+                    pop_to_check = self.traj[key].approx_pop
+#                 if self.traj[key].clone_E_diff[istate] > self.p_threshold and\
+#                 self.traj[key].populations[istate] > self.pop_threshold:
                 if self.traj[key].clone_E_diff[istate] > self.p_threshold and\
-                self.traj[key].populations[istate] > self.pop_threshold:
+                pop_to_check[istate] > self.pop_threshold and\
+                pop_to_check[istate] < 1.0 - self.pop_threshold and\
+                self.traj[key].clone_E_diff[istate] >= self.traj[key].clone_E_diff_prev[istate]:
                     print "Trajectory " + key + " cloning to ",\
                     istate, " state at time",\
                     self.traj[key].time, "with p =",\
                     self.traj[key].clone_E_diff[istate]
+                    print "pop_to_check", pop_to_check
+                    print "istate", istate
 #                         print self.p_threshold
 
                     label = str(self.traj[key].label) + "b" +\
@@ -690,9 +706,12 @@ class simulation(fmsobj):
 #                                           np.dot(self.S, self.qm_amplitudes)))
                     nuc_norm = 1.0
                     # okay, now we finally decide whether to clone or not
-                    clone_ok = newtraj.init_clone_traj_to_a_state(parent_copy,\
+                    if parent_copy.full_H:
+                        clone_ok = newtraj.init_clone_traj_to_a_state(parent_copy,\
                                                        istate, label, nuc_norm)
-                    
+                    else:
+                        clone_ok = newtraj.init_clone_traj_approx(parent_copy,\
+                                                       istate, label, nuc_norm)                    
                     if clone_ok:
                         
                         """This will be a separate subroutine eventually probably,
@@ -856,9 +875,9 @@ class simulation(fmsobj):
                         print "NORM =", norm
                         
                         # Number overlap elements that are larger than threshold
-                        s = (S > self.olapmax).sum()
+                        s = (abs(S) > self.olapmax).sum()
                         print "SUM =", s
-                        
+                        print "S =", np.abs(S[0,:])
                         if s > new_dim:
                             
                             print "Aborting cloning due to large overlap with\
@@ -942,7 +961,47 @@ class simulation(fmsobj):
                         shutil.move(filename, filename2)
         shutil.copy2("working.hdf5", "sim.hdf5")
         print "hdf5 and json output are synchronized"
+
+    def calc_approx_el_populations(self):
         
+#         n_el_states = self.traj["00"].krylov_sub_n
+#         norm = np.zeros(n_el_states)
+# 
+#         for i in range(n_el_states):
+#             c_t = self.qm_amplitudes
+#             ntraj = len(c_t)
+#             for key in self.traj:
+#                 norm[i] += np.real(np.dot(np.transpose(np.conjugate(c_t[self.traj_map[key]])),\
+#                                           c_t[self.traj_map[key]])) * self.traj[key].approx_pop[i] 
+#         print "APPROX_POP =", norm
+        n_el_states = self.traj["00"].numstates
+        norm = np.zeros(n_el_states)
+
+#         for i in range(n_el_states):
+#             c_t = self.qm_amplitudes
+#             ntraj = len(c_t)
+#             for key in self.traj:
+#                 norm[i] += np.real(np.dot(np.transpose(np.conjugate(c_t[self.traj_map[key]])),\
+#                                           c_t[self.traj_map[key]])) * self.traj[key].populations[i] 
+
+        for i in range(n_el_states):
+            nt = np.shape(self.S)[0]
+            c_t = self.qm_amplitudes
+            S_t = self.S
+            sum_pop = 0.0
+            for key1 in self.traj:
+                pop_ist = 0.0
+                ist = self.traj_map[key1]
+                for key2 in self.traj:
+                    ist2 = self.traj_map[key2]
+                    pop_ist += np.real(0.5 * (np.dot(np.conjugate(c_t[ist]),\
+                                       np.dot(S_t[ist, ist2], c_t[ist2]))\
+                                    + np.dot(np.conjugate(c_t[ist2]),\
+                                       np.dot(S_t[ist2, ist], c_t[ist])))) 
+                norm[i] += pop_ist * self.traj[key1].populations[i]
+                self.el_pop[i] = norm[i]
+        print "APPROX_POP =", norm
+    
     def h5_output(self):
         """"Writes output  to h5 file"""
         
@@ -1014,6 +1073,7 @@ class simulation(fmsobj):
         self.h5_datasets["Sdot"] = ntraj2
         self.h5_datasets["Sinv"] = ntraj2
         self.h5_datasets["num_traj_qm"] = 1
+        self.h5_datasets["el_pop"] = self.num_el_states
         
         self.h5_types = dict()
         self.h5_types["quantum_time"] = "float64"
@@ -1024,6 +1084,7 @@ class simulation(fmsobj):
         self.h5_types["Sdot"] = "complex128"
         self.h5_types["Sinv"] = "complex128"
         self.h5_types["num_traj_qm"] = "int32"
+        self.h5_types["el_pop"] = "float64"
 
     def get_qm_data_from_h5(self):
         """get the necessary geometries and energies from hdf5"""

pyspawn/traj.py
@@ -11,6 +11,7 @@ from numpy import dtype
 from pyspawn.potential.linear_slope import propagate_symplectic
 import cmath
 from scipy.optimize import fsolve, root, broyden1
+from datashape.coretypes import int32
 
 class traj(fmsobj):
         
@@ -32,7 +33,7 @@ class traj(fmsobj):
         self.timestep = 0.0
         
         self.numstates = 5
-        self.krylov_sub_n = self.numstates - 1
+        self.krylov_sub_n = self.numstates
         
         self.length_wf = self.numstates
         self.wf = np.zeros((self.numstates, self.length_wf))
@@ -51,9 +52,11 @@ class traj(fmsobj):
         #In the following block there are variables needed for ehrenfest
         self.H_elec = np.zeros((self.numstates, self.numstates), dtype = np.complex128)
         self.first_step = False
+        self.full_H = bool()
         self.new_amp = np.zeros((1), dtype = np.complex128)
         self.rescale_amp = np.zeros((1), dtype = np.complex128)
-        self.n_el_steps = 1000
+        self.n_el_steps = np.zeros((1), dtype = np.int32)
+#         self.n_el_steps = 1000
         self.td_wf_full_ts = np.zeros((self.numstates), dtype = np.complex128)
         self.td_wf = np.zeros((self.numstates), dtype = np.complex128)
         self.mce_amps = np.zeros((self.numstates), dtype = np.complex128)
@@ -63,10 +66,16 @@ class traj(fmsobj):
         self.eigenvecs = np.zeros((self.numstates, self.numstates), dtype = np.complex128)
         self.approx_eigenvecs = np.zeros((self.krylov_sub_n, self.krylov_sub_n),\
                                          dtype = np.complex128)
+        self.approx_energies = np.zeros(self.krylov_sub_n)
+        self.approx_amp = np.zeros((self.krylov_sub_n), dtype = np.complex128)
+        self.approx_pop = np.zeros(self.krylov_sub_n)
+        self.approx_wf_full_ts = np.zeros((self.krylov_sub_n), dtype = np.complex128)
+        self.wf_store_full_ts = np.zeros((self.numstates, self.krylov_sub_n),\
+                                         dtype = np.complex128)
         self.wf_store = np.zeros((self.numstates, self.krylov_sub_n), dtype = np.complex128)
-#         self.clonethresh = 0.0
         self.clone_p = np.zeros((self.numstates, self.numstates))
-        self.clone_E_diff = np.zeros(self.numstates) 
+        self.clone_E_diff = np.zeros(self.numstates)
+        self.clone_E_diff_prev = np.zeros(self.numstates)
     
     def solve_nonlin_system(self, nuc_norm, S12nuc, norm_abk, norm_abi, norm_abj, guess):
     
@@ -206,10 +215,12 @@ class traj(fmsobj):
         self.numstates = parent.numstates
         self.timestep = parent.timestep
         self.maxtime = parent.maxtime
-        
+        self.full_H = parent.full_H
         self.widths = parent.widths
         self.masses = parent.masses
-
+        self.n_el_steps = parent.n_el_steps
+        
+        
         if hasattr(parent,'atoms'):
             self.atoms = parent.atoms
         if hasattr(parent,'civecs'):
@@ -431,12 +442,216 @@ class traj(fmsobj):
         
                 else:
                     return False
+           
+    def init_clone_traj_approx(self, parent, istate, label, nuc_norm):
+        
+        self.numstates = parent.numstates
+        self.timestep = parent.timestep
+        self.maxtime = parent.maxtime
+        self.full_H = parent.full_H
+        self.n_el_steps = parent.n_el_steps
+        
+        self.widths = parent.widths
+        self.masses = parent.masses
+
+        if hasattr(parent,'atoms'):
+            self.atoms = parent.atoms
+        if hasattr(parent,'civecs'):
+            self.civecs = parent.civecs
+            self.ncivecs = parent.ncivecs
+        if hasattr(parent,'orbs'):
+            self.orbs = parent.orbs
+            self.norbs = parent.norbs
+        if hasattr(parent,'prev_wf_positions'):
+            self.prev_wf_positions = parent.prev_wf_positions
+        if hasattr(parent,'electronic_phases'):
+            self.electronic_phases = parent.electronic_phases
+        
+#         self.clonethresh = parent.clonethresh
+        self.potential_specific_traj_copy(parent)
+        
+        time = parent.time
+        self.time = time
+        self.label = label
+        pos_t = parent.positions
+        mom_t = parent.momenta_full_ts
+        tmp_pop = parent.approx_pop
+        tmp_amp = parent.approx_amp
+        tmp_force = parent.av_force
+        tmp_energy = parent.av_energy
+        eigenvals = parent.energies
+#         eigenvectors = parent.approx_eigenvecs
+        tmp_wf = parent.approx_wf_full_ts
+        
+        H_elec, Force = parent.construct_el_H(pos_t)
+        eigenvals, eigenvectors = np.linalg.eigh(H_elec)
+        
+        q, r = np.linalg.qr(parent.wf_store_full_ts)
+
+        Hk = np.dot(np.transpose(np.conjugate(q)), np.dot(H_elec, q))
+        approx_force = np.zeros((self.numdims, self.krylov_sub_n, self.krylov_sub_n),\
+                                dtype=np.complex128) 
+        for n in range(self.numdims):
+            approx_force[n] = np.dot(np.transpose(np.conjugate(q)), np.dot(Force[n], q))
+        
+        approx_e, approx_eigenvecs = np.linalg.eigh(Hk)
+        
+#         tmp_amp = np.dot(np.transpose(np.conjugate(q)), parent.mce_amps)
+#         print "approx_eigenvecs =", eigenvectors
+        print "approx_amp =", tmp_amp
+        print "approx e =", approx_e
+#       During the cloning procedure we look at pairwise decoherence times, all population
+#       is going from istate to jstate. The rest of the amplitudes change too in order to
+#       conserve nuclear norm
+    
+#         norm_abk = 0.0
+#                 
+#         for i in range(self.krylov_sub_n):
+#             if i == istate:
+#                 norm_abi = tmp_pop[i]
+#             else:
+#                 norm_abk += tmp_pop[i]
+#         print "total pop =", sum(tmp_pop)
+#         print "norm_abi =", norm_abi
+#         print "norm_abk =", norm_abk        
+        
+        child_wf = np.zeros((self.krylov_sub_n), dtype=np.complex128) 
+        parent_wf = np.zeros((self.krylov_sub_n), dtype=np.complex128) 
+        
+        for kstate in range(self.krylov_sub_n):
+            if kstate == istate:
+                # the population is removed from this state, so nothing to do here
+                child_wf += approx_eigenvecs[:, kstate] * tmp_amp[kstate] / np.abs(tmp_amp[kstate])
+
+            else:
+                # the rest of the states remain unchanged 
+                parent_wf += approx_eigenvecs[:, kstate] * tmp_amp[kstate]\
+                           / np.sqrt(1 - np.abs(tmp_amp[istate])**2)
+
+        child_wf_T = np.conjugate(np.transpose(child_wf))
+        parent_wf_T = np.conjugate(np.transpose(parent_wf))
+                       
+        child_pop = np.zeros(self.krylov_sub_n)
+        child_amp = np.zeros((self.krylov_sub_n), dtype=np.complex128) 
+        parent_pop = np.zeros(self.krylov_sub_n)
+        parent_amp = np.zeros((self.krylov_sub_n), dtype=np.complex128)         
+
+        for j in range(self.krylov_sub_n):
+            child_amp[j] = np.dot(np.conjugate(approx_eigenvecs[:, j]), child_wf)
+            child_pop[j] = np.real(np.dot(np.conjugate(child_amp[j]), child_amp[j]))
+            parent_amp[j] = np.dot(np.conjugate(approx_eigenvecs[:, j]), parent_wf)
+            parent_pop[j] = np.real(np.dot(np.conjugate(parent_amp[j]), parent_amp[j]))
+        
+        parent_force = np.zeros((self.numdims))    
+        child_force = np.zeros((self.numdims)) 
+#         print "tmp_wf", tmp_wf
+
+        for n in range(self.numdims):
+            parent_force[n] = -np.real(np.dot(np.dot(parent_wf_T, approx_force[n]), parent_wf))
+        for n in range(self.numdims):
+            child_force[n] = -np.real(np.dot(np.dot(child_wf_T, approx_force[n]), child_wf))
+        
+        child_energy = np.real(np.dot(np.dot(np.transpose(np.conjugate(child_wf)), Hk),\
+                                      child_wf))
+        parent_energy = np.real(np.dot(np.dot(np.transpose(np.conjugate(parent_wf)), Hk),\
+                                       parent_wf))
+        
+        approx_e = np.dot(np.transpose(np.conjugate(tmp_wf)), np.dot(Hk, tmp_wf))
+        exact_e = np.dot(np.transpose(np.conjugate(parent.td_wf_full_ts)), np.dot(H_elec, parent.td_wf_full_ts))
+        print "exact e =", exact_e
+        print "approx_e =", approx_e
+        print "Child E =", child_energy
+        print "Parent E =", parent_energy
+        print "child_pop =", child_pop
+        print "parent_pop", parent_pop
+#         sys.exit()
+        
+        print "Rescaling child's momentum:"
+        child_rescale_ok, child_rescaled_momenta = self.rescale_momentum(tmp_energy,\
+                                                                         child_energy,\
+                                                                         mom_t) 
+        if child_rescale_ok:
+
+            parent_E_total = tmp_energy + parent.calc_kin_en(mom_t, parent.masses)
+            child_E_total = child_energy +\
+            self.calc_kin_en(child_rescaled_momenta, self.masses)
+            print "child_E after rescale =", child_E_total
+            print "parent E before rescale=", parent_E_total 
+            print "Rescaling parent's momentum"
+            parent_rescale_ok, parent_rescaled_momenta\
+            = parent.rescale_momentum(tmp_energy, float(parent_energy), mom_t)
+            if not parent_rescale_ok:
+                return False
+            print "parent E after rescale = ",\
+            parent.calc_kin_en(parent_rescaled_momenta, parent.masses) + parent_energy         
+
+            if parent_rescale_ok:
+                # Setting mintime to current time to avoid backpropagation
+                mintime = parent.time
+                self.mintime = mintime
+                self.firsttime = time
+                self.positions = parent.positions
+    
+                # updating quantum parameters for child    
+                child_wf_orig_basis = np.dot(q, child_wf)
+                self.td_wf_full_ts = child_wf_orig_basis
+                self.td_wf = child_wf_orig_basis
+                approx_amp = np.zeros((self.numstates), dtype=np.complex128) 
+                approx_pop = np.zeros(self.numstates) 
+                for j in range(self.numstates):
+                    approx_amp[j] = np.dot(np.conjugate(np.transpose(eigenvectors[:, j])), child_wf_orig_basis)
+                    approx_pop[j] = np.real(np.dot(np.transpose(np.conjugate(approx_amp[j])), approx_amp[j]))
+                print "child_full_pop", approx_pop
+                self.av_energy = float(child_energy)
+                self.approx_amp = child_amp
+                self.approx_pop = child_pop
+                self.av_force = child_force
+                self.first_step = True
+                self.momenta = child_rescaled_momenta
+                self.momenta_full_ts = child_rescaled_momenta
+                self.eigenvecs = eigenvectors
+                self.energies = eigenvals
+                self.approx_eigenvecs = approx_eigenvecs
+                self.approx_energies = float(approx_e)
+                # IS THIS OK?!
+                self.h5_output()
+                
+                parent_wf_orig_basis = np.dot(q, parent_wf)
+                parent.momenta = parent_rescaled_momenta
+                parent.momenta_full_ts = parent_rescaled_momenta
+                parent.td_wf = parent_wf_orig_basis
+                parent.td_wf_full_ts = parent_wf_orig_basis
+                parent.av_energy = float(parent_energy)
+                parent.approx_amp = parent_amp
+                parent.approx_pop = parent_pop
+                parent.av_force = parent_force
+                parent.energies = eigenvals
+                parent.eigenvecs = eigenvectors
+                parent.approx_eigenvecs = approx_eigenvecs
+                parent.approx_energies = float(approx_e)
+                # this makes sure the parent trajectory in VV propagated as first step
+                # because the wave function is at the full TS, should be half step ahead
+                parent.first_step = True
+                print "parent wf =", parent_wf_orig_basis
+                print "AMP_i coeff=", np.abs(tmp_amp[istate])
+                print "child_pop =", child_pop
+                print "parent_pop =", parent_pop
+                self.rescale_amp[0] = np.abs(tmp_amp[istate])
+                parent.rescale_amp[0] = np.sqrt(1 - np.abs(tmp_amp[istate])**2)
+                print "Rescaling parent amplitude by a factor ", parent.rescale_amp
+                print "Rescaling child amplitude by a factor ", self.rescale_amp
+    #                 sys.exit()
+                return True
+            else:
+                return False
             
     def init_clone_traj_to_a_state(self, parent, istate, label, nuc_norm):
 
         self.numstates = parent.numstates
         self.timestep = parent.timestep
         self.maxtime = parent.maxtime
+        self.full_H = parent.full_H
+        self.n_el_steps = parent.n_el_steps
         
         self.widths = parent.widths
         self.masses = parent.masses
@@ -541,7 +756,9 @@ class traj(fmsobj):
         child_energy = np.real(np.dot(np.dot(np.transpose(np.conjugate(child_wf)), H_elec), child_wf))
         parent_energy = np.real(np.dot(np.dot(np.transpose(np.conjugate(parent_wf)),\
                                               H_elec), parent_wf))
-        
+        print "Child E =", child_energy
+        print "Parent E =", parent_energy        
+#         sys.exit()
         # Also need to update momentum 
 #         self.momenta = mom_t
         
@@ -654,6 +871,7 @@ class traj(fmsobj):
 
         # computing pairwise cloning probabilities
         self.clone_p = self.compute_cloning_probabilities()
+        self.clone_E_diff_prev = self.clone_E_diff
         self.clone_E_diff = self.compute_cloning_E_diff()
         
     def compute_cloning_E_diff(self):
@@ -661,16 +879,19 @@ class traj(fmsobj):
         
         print "Computing cloning parameters"
         
-        m = self.masses
-        ke_tot = 0.0
         clone_dE = np.zeros(self.numstates)
-        for idim in range(self.numdims):
-            ke_tot += 0.5 * self.momenta_full_ts[idim] * self.momenta_full_ts[idim] / m[idim]
-        if ke_tot > 0.0:
+        if self.full_H:
+            clone_dE = np.zeros(self.numstates)
             for istate in range(self.numstates):
                 dE = np.abs(self.energies[istate] - self.av_energy)
                 clone_dE[istate] = dE
-
+        if not self.full_H:
+            clone_dE = np.zeros(self.krylov_sub_n)
+            for istate in range(self.krylov_sub_n):
+                dE = np.abs(self.approx_energies[istate] - self.av_energy)
+                clone_dE[istate] = dE
+            print "clone_dE_approx =", clone_dE
+#             sys.exit()
         print "clone_dE =\n", clone_dE
         return clone_dE
         
@@ -729,7 +950,7 @@ class traj(fmsobj):
 
             getcom = "self." + key 
             tmp = eval(getcom)
-            
+#             print "key =", key
             if n!=1:
                 dset[ipos, 0:n] = tmp[0:n]
             else: