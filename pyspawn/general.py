bfab8b38b6fa7f3287f7ddd7779ae465add36b01
Dmitry <dfedorov@nevada.unr.edu>
2019-02-28 19:28:49 -0500

    Update splash screen to MCDMS

--- pyspawn/general.py
+++ pyspawn/general.py
@@ -2,19 +2,13 @@ import os.path
 
 def print_splash():
     print " "
-    print "You are about to propagate a molecular wave function with..."
-    print " "
-    print "                          .M\"\"\"bgd"
-    print "                         ,MI    \"Y"
-    print "    `7MMpdMAo.`7M'   `MF'`MMb.   `7MMpdMAo.  ,6\"Yb.`7M'    ,A    `MF'`7MMpMMMb."
-    print "      MM   `Wb  VA   ,V    `YMMNq. MM   `Wb 8)   MM  VA   ,VAA   ,V    MM    MM"
-    print "      MM    M8   VA ,V   .     `MM MM    M8  ,pm9MM   VA ,V  VA ,V     MM    MM"
-    print "      MM   ,AP    VVV    Mb     dM MM   ,AP 8M   MM    VVV    VVV      MM    MM"
-    print "      MMbmmd'     ,V     P\"Ybmmd\"  MMbmmd'  `Moo9^Yo.   W      W     .JMML  JMML."
-    print "      MM         ,V                MM"
-    print "    .JMML.    OOb\"               .JMML."
-    print " "
-    print "pySpawn is a nonadiabatic molecular dynamics software package written by Benjamin G. Levine"
+    print "You are about to propagate a molecular wave function with MCDMS"
+    print "\n----------------------------------------------"
+    print "Multiple Cloning in Dense Manifolds of states"
+    print "Nonadiabatic molecular dynamics software package" +\
+          "written by Benjamin G. Levine, Dmitry A. Fedorov"
+    print "------------------------------------------------"
+
     print " "
 
 def check_files():
