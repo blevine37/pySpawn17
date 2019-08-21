Fixed backprop bug, added finite temp wigner, fixed hardcoded numstates
1. Fixed the bug where the backprop_momenta was not properly set after spawning
2. Fixed the hardcoded numdims and numstates in traj object which caused issues with
   some h5 files when numdims in traj and input didn't match, now numdims and numstates
   are passed to the traj constructor. All example start files were updated
3. Added finite Temperature Wigner distribution, if initial_wigner is called as before
   the code behaves the same way. The second argument temp takes temperature in Kelvin

pyspawn/fafile.py
@@ -123,14 +123,16 @@ class fafile(object):
         of = open(filename, "w")
         t = self.datasets[times][:, 0]
         for i in range(len(t)):
-            number = np.format_float_scientific(t[i], unique=False,
-                                                precision=8)
+#             number = np.format_float_scientific(t[i], unique=False,
+#                                                 precision=8)
+            number = t[i]
             of.write(str(number) + " ")
             for iset in range(len(dsets)):
                 dat = self.datasets[dsets[iset]][i, :]
                 for j in range(len(dat)):
-                    number = np.format_float_scientific(dat[j], unique=False,
-                                                        precision=8)
+#                    number = np.format_float_scientific(dat[j], unique=False,
+#                                                        precision=8)
+                    number = dat[j]
                     of.write(str(number) + " ")
             of.write("\n")
         of.close()

pyspawn/fmsobj.py
@@ -11,70 +11,83 @@ import json
 class fmsobj(object):
     # Convert fmsobj structure to python dict structure
     def to_dict(self):
-        tempdict=(self.__dict__).copy()
+        """Convert fmsobj structure to python dict structure"""
+
+        tempdict = (self.__dict__).copy()
         for key in tempdict:
             # numpy objects
-            if type(tempdict[key]).__module__ == np.__name__ :
+            if type(tempdict[key]).__module__ == np.__name__:
                 tempdict[key] = tempdict[key].tolist()
                 for i in range(len(tempdict[key])):
                     # complex elements of 1d arrays are encoded here
                     if isinstance(tempdict[key][i], complex):
-                        tempdict[key][i] = "^complex(" + str(tempdict[key][i].real) + "," + str(tempdict[key][i].imag) + ")"
+                        tempdict[key][i] = "^complex("\
+                            + str(tempdict[key][i].real)\
+                            + "," + str(tempdict[key][i].imag) + ")"
                     else:
                         # and complex 2d arrays here
                         if isinstance(tempdict[key][i], types.ListType):
                             for j in range(len(tempdict[key][i])):
                                 if isinstance(tempdict[key][i][j], complex):
-                                    tempdict[key][i][j] = "^complex(" + str(tempdict[key][i][j].real) + "," + str(tempdict[key][i][j].imag) + ")"
+                                    tempdict[key][i][j] = "^complex(" +\
+                                        str(tempdict[key][i][j].real) + ","\
+                                        + str(tempdict[key][i][j].imag) + ")"
             # fms objects here
-            if (type(tempdict[key]).__module__)[0:7] == __name__[0:7] :
+            if (type(tempdict[key]).__module__)[0:7] == __name__[0:7]:
                 fmsobjlabel = type(tempdict[key]).__module__
                 tempdict[key] = tempdict[key].to_dict()
                 (tempdict[key])["fmsobjlabel"] = fmsobjlabel
             # dictionaries here
-            if isinstance(tempdict[key],types.DictType) :
+            if isinstance(tempdict[key], types.DictType):
                 tempdict2 = (tempdict[key]).copy()
                 tempdict[key] = tempdict2
                 for key2 in tempdict2:
-                    if (type(tempdict2[key2]).__module__)[0:7] == __name__[0:7] :
+                    if (type(tempdict2[key2]).__module__)[0:7]\
+                            == __name__[0:7]:
                         fmsobjlabel = type(tempdict2[key2]).__module__
                         tempdict2[key2] = tempdict2[key2].to_dict()
                         (tempdict2[key2])["fmsobjlabel"] = fmsobjlabel
-                
+
         return tempdict
 
-    # Convert dict structure to fmsobj structure
-    def from_dict(self,**tempdict):
+    def from_dict(self, **tempdict):
+        """Convert dict structure to fmsobj structure"""
+
         for key in tempdict:
-            if isinstance(tempdict[key],types.UnicodeType) :
+            if isinstance(tempdict[key], types.UnicodeType):
                 tempdict[key] = str(tempdict[key])
-            if isinstance(tempdict[key],types.ListType) :
-                if isinstance((tempdict[key])[0],types.FloatType) :
+            if isinstance(tempdict[key], types.ListType):
+                if isinstance((tempdict[key])[0], types.FloatType):
                     # convert 1d float lists to np arrays
-                    tempdict[key] = np.asarray(tempdict[key],dtype=np.float64)
-                if isinstance((tempdict[key])[0],types.StringTypes) :
+                    tempdict[key] = np.asarray(tempdict[key],
+                                               dtype=np.float64)
+                if isinstance((tempdict[key])[0], types.StringTypes):
                     if (tempdict[key])[0][0] == "^":
                         for i in range(len(tempdict[key])):
-                            tempdict[key][i] = eval(tempdict[key][i])
-                        tempdict[key] = np.asarray(tempdict[key],dtype=np.complex128)
-#new
-                    if isinstance((tempdict[key])[0],types.UnicodeType) :
+                            tempdict[key][i] = eval(tempdict[key][i][1:])
+                        tempdict[key] = np.asarray(tempdict[key],
+                                                   dtype=np.complex128)
+                    # new
+                    if isinstance((tempdict[key])[0], types.UnicodeType):
                         for i in range(len(tempdict[key])):
-                            tempdict[key][i]= str(tempdict[key][i])
-#end new
-                            
-                            
+                            tempdict[key][i] = str(tempdict[key][i])
+                    # end new
+
                 else:
-                    if isinstance((tempdict[key])[0],types.ListType):
-                        if isinstance((tempdict[key])[0][0],types.FloatType) :
+                    if isinstance((tempdict[key])[0], types.ListType):
+                        if isinstance((tempdict[key])[0][0], types.FloatType):
                             # convert 2d float lists to np arrays
-                            tempdict[key] = np.asarray(tempdict[key],dtype=np.float64)
-                        if isinstance((tempdict[key])[0][0],types.StringTypes) :
+                            tempdict[key] = np.asarray(tempdict[key],
+                                                       dtype=np.float64)
+                        if isinstance((tempdict[key])[0][0],
+                                      types.StringTypes):
                             if (tempdict[key])[0][0][0] == "^":
                                 for i in range(len(tempdict[key])):
                                     for j in range(len(tempdict[key][i])):
-                                        tempdict[key][i][j] = eval(tempdict[key][i][j][1:])
-                                tempdict[key] = np.asarray(tempdict[key],dtype=np.complex128)
+                                        tempdict[key][i][j]\
+                                            = eval(tempdict[key][i][j][1:])
+                                tempdict[key] = np.asarray(tempdict[key],
+                                                           dtype=np.complex128)
         self.__dict__.update(tempdict)
 
     # Write fmsobj structure to disk in json format

pyspawn/traj.py
@@ -8,13 +8,13 @@ import shutil
 import h5py
 
 class traj(fmsobj):
-    def __init__(self):
+    def __init__(self, numdims, numstates):
         self.time = 0.0
         self.time_half_step = 0.0
         self.maxtime = -1.0
         self.mintime = 0.0
         self.firsttime = 0.0
-        self.numdims = 2
+        self.numdims = numdims
         self.positions = np.zeros(self.numdims)
         self.momenta = np.zeros(self.numdims)
         self.widths = np.zeros(self.numdims)
@@ -26,8 +26,9 @@ class traj(fmsobj):
 
         self.timestep = 0.0
         #self.propagator = "vv"
-        
-        self.numstates = 2
+        # THIS NEEDS TO BE REWRITTEN, causes issues when numstates hardcoded in here 
+        # DOESN'T MATCH the numstates in the input
+        self.numstates = numstates
         #self.software = "pyspawn"
         #self.method = "cone"
         self.length_wf = self.numstates
@@ -446,8 +447,10 @@ class traj(fmsobj):
         pos = parent.get_positions_tmdt()
         mom = parent.get_momenta_tmdt()
         e = parent.get_energies_tmdt()
-
-# adjust momentum
+        print "Spawning new trajectory:"
+        print "mom =", mom
+        print "pos =", pos
+        print "e =", e
         
         self.set_positions(pos)
         self.set_momenta(mom)
@@ -459,7 +462,7 @@ class traj(fmsobj):
         #print "init_st time", time
         self.set_backprop_time(time)
         self.set_backprop_positions(pos)
-        self.set_backprop_momenta(mom)
+        #self.set_backprop_momenta(mom)
         #print "init_st mintime2", self.get_mintime()
 
         self.set_firsttime(time)
@@ -539,12 +542,13 @@ class traj(fmsobj):
         self.potential_specific_traj_copy(existing)
 
     def rescale_momentum(self, v_parent):
+        """ Computing kinetic energy of parent.  Remember that, at this point,
+        the child's momentum is still that of the parent, so we compute
+        t_parent from the child's momentum"""
+
         v_child = self.get_energies()[self.get_istate()]
         #print "rescale v_child ", v_child
         #print "rescale v_parent ", v_parent
-        # computing kinetic energy of parent.  Remember that, at this point,
-        # the child's momentum is still that of the parent, so we compute
-        # t_parent from the child's momentum
         p_parent = self.get_momenta()
         m = self.get_masses()
         t_parent = 0.0
@@ -556,13 +560,12 @@ class traj(fmsobj):
             print "# Aborting spawn because child does not have"
             print "# enough energy for momentum adjustment"
             return False
-        #print "# rescaling momentum by factor ", factor
         factor = math.sqrt(factor)
         print "# rescaling momentum by factor ", factor
         p_child = factor * p_parent
-        #print "rescale p_child ", p_child
-        #print "rescale p_parent ", p_parent
         self.set_momenta(p_child)
+        self.set_backprop_momenta(p_child)
+
         return True
 
     def set_forces(self,f):
@@ -872,7 +875,7 @@ class traj(fmsobj):
             all_datasets.update(self.h5_datasets_half_step)
         for key in all_datasets:
             n = all_datasets[key]
-            #print "key", key
+            #print "key =", key
             dset = trajgrp.get(key)
             l = dset.len()
             dset.resize(l+1,axis=0)
@@ -882,8 +885,9 @@ class traj(fmsobj):
                 ipos=0
                 dset[1:(l+1),0:n] = dset[0:(l),0:n]
             getcom = "self.get_" + cbackprop + key + "()"
-            #print getcom
+            #print "getcom =", getcom
             tmp = eval(getcom)
+            #print "ipos =", ipos
             if n!=1:
                 dset[ipos,0:n] = tmp[0:n]
             else:
@@ -1036,8 +1040,9 @@ class traj(fmsobj):
         #print "tdc", tdc
         return tdc
 
-    def initial_wigner(self,iseed):
-        print "## randomly selecting Wigner initial conditions"
+    def initial_wigner(self, iseed, temp=0.0):
+        print "## randomly selecting Wigner initial conditions at T=", temp
+        beta = 1 / (temp * 0.000003166790852)
         ndims = self.get_numdims()
 
         h5f = h5py.File('hessian.hdf5', 'r')
@@ -1054,10 +1059,10 @@ class traj(fmsobj):
         h_mw = np.zeros_like(h)
 
         for idim in range(ndims):
-            h_mw[idim,:] = h[idim,:] / sqrtm
+            h_mw[idim, :] = h[idim, :] / sqrtm
 
         for idim in range(ndims):
-            h_mw[:,idim] = h_mw[:,idim] / sqrtm
+            h_mw[:, idim] = h_mw[:, idim] / sqrtm
 
         # symmetrize mass weighted hessian
         h_mw = 0.5 * (h_mw + h_mw.T)
@@ -1075,21 +1080,22 @@ class traj(fmsobj):
         
         # seed random number generator
         np.random.seed(iseed)
-        
-        alphax = np.sqrt(evals[0:ndims-6])/2.0
-
+        alphax = np.sqrt(evals[0:ndims-6]) / 2.0
+        # finite temperature distribution
+        if beta > 1e-05:
+            alphax = alphax * np.tanh(np.sqrt(evals[0:ndims-6]) * beta / 2)
         sigx = np.sqrt(1.0 / ( 4.0 * alphax))
         sigp = np.sqrt(alphax)
 
         dtheta = 2.0 * np.pi * np.random.rand(ndims-6)
-        dr = np.sqrt( np.random.rand(ndims-6) )
+        dr = np.sqrt(np.random.rand(ndims-6))
 
-        dx1 = dr * np.sin(dtheta)
+        dx1 = dr * np.sin(dtheta) 
         dx2 = dr * np.cos(dtheta)
 
         rsq = dx1 * dx1 + dx2 * dx2
 
-        fac = np.sqrt( -2.0 * np.log(rsq) / rsq )
+        fac = np.sqrt(-2.0 * np.log(rsq) / rsq)
 
         x1 = dx1 * fac
         x2 = dx2 * fac
@@ -1097,16 +1103,17 @@ class traj(fmsobj):
         posvec = np.append(sigx * x1, np.zeros(6))
         momvec = np.append(sigp * x2, np.zeros(6))
 
-        deltaq = np.matmul(modes,posvec)/sqrtm
+        deltaq = np.matmul(modes, posvec) / sqrtm
         pos += deltaq
-        mom = np.matmul(modes,momvec)*sqrtm
+        mom = np.matmul(modes, momvec) * sqrtm
 
         self.set_positions(pos)
         self.set_momenta(mom)
 
         zpe = np.sum(alphax[0:ndims-6])
         ke = 0.5 * np.sum(mom * mom / m)
-
+        #print np.sqrt(np.tanh(evals[0:ndims-6]/(2*0.0031668)))
+        print "beta = ", beta
         print "# ZPE = ", zpe
         print "# kinetic energy = ", ke
 