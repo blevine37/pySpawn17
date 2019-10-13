2bc343f9221d885c66abbe54308d17d1e4ab13ee
Dmitry <dfedorov@nevada.unr.edu>
2019-10-12 21:51:27 -0400

    Added requires section

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#from setuptools import setup
+# from setuptools import setup
 from distutils.core import setup
 
 setup(name='pyspawn',
@@ -6,6 +6,7 @@ setup(name='pyspawn',
       description='Full Multiple Spawning in Python',
       author='Benjamin G. Levine',
       url='https://github.com/blevine37/pySpawn17',
-      packages=['pyspawn', 'pyspawn.classical_integrator', 'pyspawn.potential', 'pyspawn.qm_hamiltonian', 'pyspawn.qm_integrator'],
-      #install_requires=['numpy', 'h5py']
+      packages=['pyspawn', 'pyspawn.classical_integrator', 'pyspawn.potential', 'pyspawn.qm_hamiltonian',
+                'pyspawn.qm_integrator'], requires=['numpy', 'h5py']
+      # install_requires=['numpy', 'h5py']
       )
