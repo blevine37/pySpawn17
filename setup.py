f31f53e550018cea8258012c59893a7d175ef109
Dmitry <dfedorov@nevada.unr.edu>
2019-12-04 09:32:58 -0500

    Putting plotting functions into pyspawn to avoid messiness

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(name='pyspawn',
       author='Benjamin G. Levine',
       url='https://github.com/blevine37/pySpawn17',
       packages=['pyspawn', 'pyspawn.classical_integrator', 'pyspawn.potential', 'pyspawn.qm_hamiltonian',
-                'pyspawn.qm_integrator'], requires=['numpy', 'h5py']
+                'pyspawn.qm_integrator', 'pyspawn.plotting'], requires=['numpy', 'h5py']
       # install_requires=['numpy', 'h5py']
       )
