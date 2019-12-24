be10f90918ddd64d7bc32e3eaf6fb2b448a08722
Dmitry Fedorov <fedorovd@corecm.cem.msu.edu>
2019-12-23 20:35:45 -0500

    Fixed plotting functions to use dictionary for istates

--- pyspawn/plotting/traj_plot.py
+++ pyspawn/plotting/traj_plot.py
@@ -6,7 +6,7 @@ import h5py
 import glob
 
 
-def plot_total_energies(time, toten, all_keys, keys_to_plot, istates, colors, markers, linestyles):
+def plot_total_energies(time, toten, keys, istates_dict, colors, markers, linestyles):
     """Plots total classical energies for each trajectory and saves it to png file.
     This plot is very useful to check if energy is conserved.
     Color is represented by the istate of a trajectory
@@ -16,20 +16,12 @@ def plot_total_energies(time, toten, all_keys, keys_to_plot, istates, colors, ma
     ax = plt.figure("Total Energies")
     min_energy = min(toten["00"])
     max_energy = max(toten["00"])
-    for index, key in enumerate(all_keys):
-        # since we have an array istates that corresponds to keys array
-        # we need to make sure that when we need to plot only some of the trajectories we
-        # don't confuse the istates
-        if key in keys_to_plot:
-            if len(all_keys) > 1:
-                plt.plot(time[key], toten[key], label=key, color=colors[int(istates[index])],
-                     linestyle=linestyles[index], marker=markers[index])
-            else:
-                plt.plot(time[key], toten[key], label=key, color=colors[int(istates)],
-                     linestyle=linestyles[index], marker=markers[index])
-            if min(toten[key]) < min_energy:
+    for index, key in enumerate(keys):
+        plt.plot(time[key], toten[key], label=key, color=colors[int(istates_dict[key])],
+                 linestyle=linestyles[index], marker=markers[index])
+        if min(toten[key]) < min_energy:
                 min_energy = min(toten[key])
-            if max(toten[key]) > max_energy:
+        if max(toten[key]) > max_energy:
                 max_energy = max(toten[key])
 
     plt.xlabel('Time, au')
@@ -59,25 +51,19 @@ def plot_total_pop(time, el_pop, nstates, colors):
     g5.savefig("Total_El_pop.png", dpi=300)
 
 
-def plot_e_gap(time, poten, all_keys, keys_to_plot, state1, state2, istates, colors, linestyles, markers):
+def plot_e_gap(time, poten, keys, state1, state2, istates_dict, colors, linestyles, markers):
     """Plots gaps between the specified states for all trajectories
     istates order needs to be fixed!
     """
 
     g2 = plt.figure("Energy gap")
 
-    for index, key in enumerate(all_keys):
-        if key in keys_to_plot:
-            if len(all_keys) > 1:
-                plt.plot(time[key], poten[key][:, state2] - poten[key][:, state1], linestyle=linestyles[index],
-                         marker=markers[index], color=colors[int(istates[index])],
-                         label=key + ": " + r'$S_{}$'.format(state2) + "-"
-                               + r'$S_{}$'.format(state1))
-            else:
-                plt.plot(time[key], poten[key][:, state2] - poten[key][:, state1], linestyle=linestyles[index],
-                         marker=markers[index], color=colors[int(istates)],
-                         label=key + ": " + r'$S_{}$'.format(state2) + "-"
-                           + r'$S_{}$'.format(state1))
+    for index, key in enumerate(keys):
+        plt.plot(time[key], poten[key][:, state2] - poten[key][:, state1], linestyle=linestyles[index],
+                 marker=markers[index], color=colors[int(istates_dict[key])],
+                 label=key + ": " + r'$S_{}$'.format(state2) + "-"
+                 + r'$S_{}$'.format(state1))
+
     plt.xlabel('Time, au')
     plt.title('Energy gaps, au')
     plt.ylabel('Energy gap, au')
@@ -92,7 +78,7 @@ def plot_energies(keys, time, poten, numstates, colors, linestyles):
     for index_key, key in enumerate(keys):
         for index_state, n in enumerate(range(numstates)):
             plt.plot(time[key], poten[key][:, n],
-                     label=key + ": " + str((n + 1)) + "state", linestyle=linestyles[index_key],
+                     label=key + ": " + 'S' + str((n + 1)), linestyle=linestyles[index_key],
                      color=colors[index_state])
         plt.xlabel('Time, au')
         plt.ylabel('Energy, au')
@@ -100,19 +86,25 @@ def plot_energies(keys, time, poten, numstates, colors, linestyles):
         g3.savefig("Energies.png", dpi=300)
 
 
-def plot_tdc(time, tdc, labels, numstates, spawnthresh):
+def plot_tdc(time, tdc, keys, numstates, istates_dict, spawnthresh, colors, linestyles, markers):
 
     plt.figure("Time-derivative couplings")
-    for key in labels:
+
+    for index_key, key in enumerate(keys):
+        # we only plot subset of trajectories but need all of them to match istates with labels
         for n in range(numstates):
-            plt.plot(time[key], np.abs(tdc[key][:, n]))
+            if n != int(istates_dict[key]):
+                # we don't plot coupling with itself which is zero
+                plt.plot(time[key][:len(tdc[key])], np.abs(tdc[key][:, n]),
+                         color=colors[istates_dict[key]], linestyle=linestyles[index_key],
+                         marker=markers[index_key])
 
     plt.axhline(y=spawnthresh, alpha=0.5, color='r', linewidth=1.0,
                 linestyle='--')
     plt.xlabel('Time, au')
     plt.ylabel('Coupling, au')
 #     for m in range(nstates):
-#         plt.text(spawn_times[m], plt.ylim()[1]-0.05, labels[m], fontsize=10)
+#         plt.text(spawn_times[m], plt.ylim()[1]-0.05, all_keys[m], fontsize=10)
 
     plt.title('Time-derivative couplings, thresh='+str(spawnthresh))
 #     plt.tight_layout
